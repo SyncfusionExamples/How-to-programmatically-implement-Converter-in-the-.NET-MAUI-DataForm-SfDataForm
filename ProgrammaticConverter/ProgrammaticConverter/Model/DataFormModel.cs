95c6d975a599a70e3fe695fbb47c9bded16e76b4
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2024-01-02 17:03:51 +0530

    Added sample.

---
+++ ProgrammaticConverter/ProgrammaticConverter/Model/DataFormModel.cs
@@ -0,0 +1,17 @@
+﻿using Syncfusion.Maui.DataForm;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammaticConverter
+{
+    public class DataFormModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataFormValueConverter(typeof(Converter))]
+        [DataType(DataType.Date)]
+        public string DateTime { get; set; }
+        public string Email { get; set; }
+        public string CourseCompleted { get; set; }
+    }
+}
